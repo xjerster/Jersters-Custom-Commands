9a8e0163a3dbdde29708a026326ad8aa03147270
Jerster <xxxjerster@gmail.com>
2025-04-21 14:27:02 +0100

    Added more blasted lands buff trade items, and Stormwind fit collection trade items

--- JerstersCustomCommands/TradeConfigs.lua
+++ JerstersCustomCommands/TradeConfigs.lua
@@ -1,5 +1,5 @@
 
--- Configuration table for Darnassus trade items
+-- Configuration table for Darnassus gift collection trade items
 local DarnassusConfig = {
     items = {
         {name = "Handmade Woodcraft", stackSize = 5}, -- Per-item stack size
@@ -8,7 +8,19 @@ local DarnassusConfig = {
     },
     stacksToTrade = 1,
     validStacks = {1, 2, 3, 4}, -- Allowed # of stacks to trade.
-    helpText = "Automates trading Darnassus Friendship materials"
+    helpText = "Automates trading Darnassus gift collection trade items"
+}
+
+-- Configuration table for Stormwind gift collection trade items
+local StormwindConfig = {
+    items = {
+        {name = "Stormwind Guard's Card", stackSize = 5}, -- Per-item stack size
+        {name = "Homemade Bread", stackSize = 5},
+        {name = "Pledge of Loyalty: Stormwind", stackSize = 5}
+    },
+    stacksToTrade = 1,
+    validStacks = {1, 2, 3, 4}, -- Allowed # of stacks to trade.
+    helpText = "Automates trading Stormwind gift collection trade items"
 }
 
 -- Configuration table for Scropok
@@ -19,12 +31,50 @@ local ScorpokConfig = {
         {name = "Vulture Gizzard", stackSize = 2}
     },
     stacksToTrade = 1,
-    validStacks = {1, 2, 3, 4}, -- Allowed # of stacks to trade.
-    helpText = "Automates trading Salt of the Scorpok materials"
+    validStacks = {1, 2, 3, 4, 5, 6}, -- Allowed # of stacks to trade.
+    helpText = "Automates trading Salt of the Scorpok quest materials"
+}
+
+-- Configuration table for ROIDS
+local ROIDSConfig = {
+    items = {
+        {name = "Scorpok Pincer", stackSize = 1}, -- Per-item stack size
+        {name = "Blasted Boar Lung", stackSize = 2},
+        {name = "Snickerfang Jowl", stackSize = 3}
+    },
+    stacksToTrade = 1,
+    validStacks = {1, 2, 3, 4, 5, 6}, -- Allowed # of stacks to trade.
+    helpText = "Automates trading Salt of R.O.I.D.S. quest materials"
+}
+
+-- Configuration table for Gizzard Gum
+local GizzardConfig = {
+    items = {
+        {name = "Vulture Gizzard", stackSize = 10}, -- Per-item stack size
+        {name = "Snickerfang Jowl", stackSize = 2}
+    },
+    stacksToTrade = 1,
+    validStacks = {1, 2,}, -- Allowed # of stacks to trade.
+    helpText = "Automates trading Gizzard Gum quest materials"
+}
+
+-- Configuration table for Infallible Mind
+local MindConfig = {
+    items = {
+        {name = "Basilisk Brains", stackSize = 10}, -- Per-item stack size
+        {name = "Vulture Gizzard", stackSize = 2}
+    },
+    stacksToTrade = 1,
+    validStacks = {1, 2,}, -- Allowed # of stacks to trade.
+    helpText = "Automates trading Infallible Mind quest materials"
 }
 
 -- Register slash commands
 TradeItems:RegisterSlashCommand("JCCDARNBUFF", DarnassusConfig)
+TradeItems:RegisterSlashCommand("JCCSWBUFF", StormwindConfig)
 TradeItems:RegisterSlashCommand("JCCSCORPOK", ScorpokConfig)
+TradeItems:RegisterSlashCommand("JCCROIDS", ROIDSConfig)
+TradeItems:RegisterSlashCommand("JCCGUM", GizzardConfig)
+TradeItems:RegisterSlashCommand("JCCMIND", MindConfig)
 -- Register /JCCTrade
 TradeItems:RegisterCustomTradeCommand("JCCTRADE")
